1234ae166e7b3da45def6e0cbd1c3903d5f72fa0
Rudi Horn <rhorn@fen-qws-int2.delacy.com>
2018-10-18 10:17:44 +0100

    updated memoization function to do cycle detection and waiting

--- src/memoization.mli
+++ src/memoization.mli
@@ -10,9 +10,10 @@ type 'a input_spec = {
   not_equal : 'a -> 'a -> bool;
 }
 
-val string_input_spec : string input_spec;;
-val dummy_input_spec : 'a input_spec;;
-val path_input_spec : Path.t input_spec;;
+val string_input_spec : string input_spec
+val int_input_spec : int input_spec
+val dummy_input_spec : 'a input_spec
+val path_input_spec : Path.t input_spec
 val pair_l_input_spec : 'a input_spec -> 'b input_spec -> ('a * 'b) input_spec
 val pair_r_input_spec : 'a input_spec -> 'b input_spec -> ('a * 'b) input_spec
 
@@ -42,5 +43,9 @@ module Memoize : sig
 
   (* add a dependency to the current fiber context *)
   val add_dep : name -> ser_input -> 'a -> 'a Fiber.t
+
+  val dump_stack : 'a -> 'a Fiber.t
+
+  val get_call_stack : (name * ser_input) list Fiber.t
 end
 

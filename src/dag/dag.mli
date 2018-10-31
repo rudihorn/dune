dc600b3e4dc543ad8e287f5fdc00154f79073737
Rudi Horn <rhorn@vdc-qws-i20304a.delacy.com>
2018-10-31 11:56:27 +0000

    separated out dag code and added a unit test

---
+++ src/dag/dag.mli
@@ -0,0 +1,17 @@
+open! Stdune
+
+type 'a t
+
+type 'a node 
+
+val create : unit -> 'a t
+
+val node : 'a t -> 'a -> 'a node
+
+val add : 'a node -> 'a node -> unit
+
+val get : 'a node -> 'a
+
+val children : 'a node -> 'a node list
+
+val pp : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a node -> unit

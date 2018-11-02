efcb53d2f5cfe31c9c5ae8f21a9ac5e0a3a60153
Rudi Horn <dyn-git@rudi-horn.de>
2018-11-02 16:47:22 +0000

    updated comments in mli files
    Signed-off-by: Rudi Horn <dyn-git@rudi-horn.de>

--- src/stdune/id.mli
+++ src/stdune/id.mli
@@ -2,8 +2,12 @@ open !Set
 
 type t
 
+(** Convert an ID to an integer. *)
 val to_int : t -> int
 
+(** The internal IdGen module which generates an
+    id using an instance of an ID generator for
+    the next ID. *)
 module IdGen : sig
   type idgen 
 
@@ -17,9 +21,17 @@ module Set : Set_intf.S with type elt = t
 
 module type IdMod = sig
   val idgen : IdGen.idgen
+
+  (** Generate a new id. *)
   val gen : unit -> t
+
+  (** Get the next id that would be generated, without actually
+      generating it. *)
   val peek : unit -> t
+
+  (** Convert the id to an integer. *)
   val to_int : t -> int
 end
 
+(** A functor to create a new ID generator module. *)
 module Make () : IdMod
